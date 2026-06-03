b93c8cc7c70e5a509a8e2841e599898aad196177
Siriskarsvag <siri.skarsvaag@hotmail.com>
2026-06-03 19:22:50 +0200

    Changed CSS keyframe animation to JS functions
    replaced CCS-based animation with JavaScript carousel. Added buttons to navigate to next event for moreuser interaction

---
+++ main.js
@@ -0,0 +1,58 @@
+const events = [
+    {
+        title: "Exclusive Energica Exhibition",
+        image: "/images/scientific-2040795_1280.jpg",
+    },
+    {
+        title: "Visiting Professor in Aeronautics",
+        image: "/images/andrew-ruiz-348421.jpg",
+    },
+    {
+        title: "Holidays Club - Explore the North Star",
+        image: "/images/marek-piwnicki-iwabZE-qN_U-unsplash.jpg"
+    }
+];
+
+let currentIndex = 0;
+
+const title = document.getElementById("event-title");
+const hero = document.querySelector(".event-cards");
+
+function updateEvent() {
+    title.textContent = events[currentIndex].title;
+
+    hero.style.backgroundImage = `url(${events[currentIndex].image})`;
+}
+
+function nextEvent() {
+    currentIndex++;
+
+    if (currentIndex >= events.length) {
+        currentIndex = 0;
+    }
+
+    updateEvent();
+}
+
+function prevEvent() {
+    currentIndex--;
+
+    if (currentIndex >= events.length) {
+        currentIndex = 0;
+    }
+
+    updateEvent();
+}
+
+document.getElementById("nextBtn").addEventListener("click", nextEvent);
+
+document.getElementById("prevBtn").addEventListener("click", prevEvent);
+
+setInterval(nextEvent, 5000);
+
+updateEvent();
+
+viewActivityBtn = document.getElementById("viewAllBtn");
+viewActivityBtn.addEventListener("click", () => {
+    window.location.href = "activities-page.html";
+});
